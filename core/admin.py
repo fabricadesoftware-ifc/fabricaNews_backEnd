f00a8a33dcc2a643f50ba6ab82503f4b5bb6acec
--global <--global>
2023-04-14 16:16:08 -0300

    Create 'feeling' model #4

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from core.models import Category
+from core.models import Category, Feeling
 
 admin.site.register(Category)
+admin.site.register(Feeling)
---
+++ core/models/feeling.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Feeling(models.Model):
+    description = models.CharField(max_length=50, null=False, blank=False)
+    emoji = models.CharField(max_length=300, null=False, blank=False)
+
+    def __str__(self):
+        return self.description
