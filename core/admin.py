071e759b58a687d939cc5ff581860b85a09cfe57
Fábio Longo de Moura <ldmfabio@gmail.com>
2023-04-17 15:41:53 -0300

    models #1 #10 #3 #4 #11 #7 #10

--- core/admin.py
+++ core/admin.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
 
-from core.models import Category, Feeling, User, NewsFeel, Comments
-# News
+from core.models import Category, Feeling, User, NewsFeel, Comments, News
 
 admin.site.register(Category)
 admin.site.register(Feeling)
 admin.site.register(User)
 admin.site.register(NewsFeel)
-# admin.site.register(News)
+admin.site.register(News)
 admin.site.register(Comments)
--- core/models/__init__.py
+++ core/models/__init__.py
@@ -2,5 +2,5 @@ from .category import Category
 from .feeling import Feeling
 from .user import User
 from .news_feel import NewsFeel
-# from .news import News
+from .news import News
 from .comments import Comments
