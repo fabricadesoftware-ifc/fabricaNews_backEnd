d5fbea908da556be5ad69290cdd87047868ad583
LucasLutf <oolucaslutf@gmail.com>
2023-04-17 12:40:30 -0300

    Create comments model #11

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from core.models import Category, Feeling, User
+from core.models import Category, Feeling, User, Comments
 
 admin.site.register(Category)
 admin.site.register(Feeling)
 admin.site.register(User)
+admin.site.register(Comments)
 
--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,3 +1,4 @@
 from .category import Category
 from .feeling import Feeling
 from .user import User
+from .comments import Comments
