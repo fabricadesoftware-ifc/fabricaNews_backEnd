416dee31d22706f7b024f90d8cddd0ae791a48c3
Davi <davigabtomaz@gmail.com>
2023-04-24 15:02:52 -0300

    rework model Category #40

--- core/models/category.py
+++ core/models/category.py
@@ -1,7 +1,8 @@
 from django.db import models
 
 class Category(models.Model):
-    description = models.CharField(max_length=50, null='False', blank='False')
+    description = models.CharField(max_length=50, null=False, blank=False)
+    user_follow = models.JSONField(null=True, blank=True)
 
     def __str__(self):
         return self.description
