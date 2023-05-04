84b5fde93d26259690a1427cf495906f713e959d
ldmfabio <ldmfabio@gmail.com>
2023-05-04 16:49:24 -0300

    Adjust naming convention #35

--- core/serializers/saveToRead.py
+++ core/serializers/saveToRead.py
@@ -1,7 +1,7 @@
 from rest_framework.serializers import ModelSerializer
 from core.models import saveToRead
 
-class Save_to_readSerializer(ModelSerializer):
+class SaveToReadSerializer(ModelSerializer):
     class Meta:
         model = saveToRead
         fields = "__all__"
