c384271cdc56602c593ce7374458995f56e78c92
ldmfabio <ldmfabio@gmail.com>
2023-05-04 16:52:53 -0300

    Adjust naming convention #35

--- core/views/saveToRead.py
+++ core/views/saveToRead.py
@@ -1,8 +1,8 @@
 from rest_framework.viewsets import ModelViewSet
 from core.models import SaveToRead
-from core.serializers import Save_to_readSerializer
+from core.serializers import SaveToReadSerializer
 
 
 class SaveToReadViewSet(ModelViewSet):
     queryset = SaveToRead.objects.all()  # pylint: disable=E1101
-    serializer_class = Save_to_readSerializer
+    serializer_class = SaveToReadSerializer
